f33b1dba981395c78efd1c1473e4b6dd682ac4aa
Rafael-99-spec <60171771+Rafael-99-spec@users.noreply.github.com>
2020-05-21 13:46:45 -0700

    Update NFS-client.sh

--- NFS-client.sh
+++ NFS-client.sh
@@ -1,6 +1,8 @@
 sudo yum -y install nfs-utils
-sudo echo "Domain = srv.world" >> /etc/idmapd.conf
+sudo chmod 777 /etc/idmapd.conf
+sudo echo "Domain = localdomain" >> /etc/idmapd.conf
 sudo systemctl start rpcbind
 sudo systemctl enable rpcbind
-sudo mount -t nfs dlp.srv.world:/home /home
-sudo echo "dlp.srv.world:/home  /home                   nfs     defaults        0 0" >> /etc/fstab
+sudo mount -t nfs 192.168.11.101:/home /home
+sudo chmod 777 /etc/fstab
+sudo echo "192.168.11.101:/home  /home                   nfs     defaults        0 0" >> /etc/fstab
