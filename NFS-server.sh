f6877b985f9de971677db670f02d85d3ad7895e0
Rafael-99-spec <60171771+Rafael-99-spec@users.noreply.github.com>
2020-05-21 13:39:10 -0700

    Update NFS-server.sh

--- NFS-server.sh
+++ NFS-server.sh
@@ -1,8 +1,12 @@
 sudo yum -y install nfs-utils
+sudo chmod 777 /etc/idmapd.conf
+sudo chmod 777 /etc/exports
 sudo echo "Domain = srv.world" >> /etc/idmapd.conf 
 sudo echo "/home 192.168.11.0/24(rw,no_root_squash)" >> /etc/exports
 sudo systemctl start rpcbind nfs-server
 sudo systemctl enable rpcbind nfs-server
+sudo systemctl start firewalld
+sudo systemctl enable firewalld
 sudo firewall-cmd --add-service=nfs --permanent
 sudo firewall-cmd --add-service={nfs3,mountd,rpc-bind} --permanent
 sudo firewall-cmd --reload
