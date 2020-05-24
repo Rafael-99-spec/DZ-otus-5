35d7a883132f7cbfd64129eb335f1adf420e18a5
Rafael-99-spec <60171771+Rafael-99-spec@users.noreply.github.com>
2020-05-24 20:05:22 +0200

    Update NFS-client.sh

--- NFS-client.sh
+++ NFS-client.sh
@@ -1,6 +1,5 @@
 sudo yum -y install nfs-utils
 sudo chmod 777 /etc/idmapd.conf
-sudo echo "Domain = localdomain" >> /etc/idmapd.conf
 sudo systemctl start rpcbind
 sudo systemctl enable rpcbind
 sudo mount -t nfs 192.168.11.101:/home /home
