6bf163d2fa8303a1004ca845326c276a306484e6
Rafael-99-spec <60171771+Rafael-99-spec@users.noreply.github.com>
2020-05-26 19:52:38 +0200

    Update NFS-client.sh

--- NFS-client.sh
+++ NFS-client.sh
@@ -4,5 +4,5 @@ sudo systemctl enable rpcbind
 cd /
 sudo mkdir /mount
 sudo chmod 777 /etc/fstab
-sudo echo "192.168.11.101:/mount  /mount		nfs       x-systemd.automount,x-systemd.device-timeout=10,timeo=14,hard,intr,noatime 0 0" >> /etc/fstab
+sudo echo "192.168.11.101:/mount  /mount		nfs        _netdev,x-systemd.automount,x-systemd.requires=network-online.target,x-systemd.device-timeout=10 0 0" >> /etc/fstab
 sudo mount -t nfs 192.168.11.101:/mount /mount
