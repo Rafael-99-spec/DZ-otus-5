fe4e6d19491e3a6f660fdfc34b11d39d08ab1eec
Rafael-99-spec <60171771+Rafael-99-spec@users.noreply.github.com>
2020-05-26 07:07:28 -0700

    Update NFS-server.sh

--- NFS-server.sh
+++ NFS-server.sh
@@ -1,7 +1,11 @@
 sudo yum -y install nfs-utils
 sudo chmod 777 /etc/idmapd.conf
 sudo chmod 777 /etc/exports
-sudo echo "/home 192.168.11.0/24(rw,no_root_squash)" >> /etc/exports
+cd /
+sudo mkdir -p /mount/upload
+sudo chmod 777 /mount
+sudo chmod 777 /mount/upload
+sudo echo "/mount/upload 192.168.11.0/24(rw,no_root_squash)" >> /etc/exports
 sudo systemctl start rpcbind nfs-server
 sudo systemctl enable rpcbind nfs-server
 sudo systemctl start firewalld
