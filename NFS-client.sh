135ec029433322581a4f47837699cdcf23f8c084
Rafael-99-spec <60171771+Rafael-99-spec@users.noreply.github.com>
2020-05-27 01:53:24 -0700

    Update NFS-client.sh

--- NFS-client.sh
+++ NFS-client.sh
@@ -6,10 +6,3 @@ sudo mkdir /mount
 sudo chmod 766 /etc/fstab
 sudo echo "192.168.11.101:/mount  /mount		nfs        _netdev,x-systemd.automount,x-systemd.requires=network-online.target,x-systemd.device-timeout=10 0 0" >> /etc/fstab
 sudo mount -t nfs 192.168.11.101:/mount /mount
-sudo sed -i '5d' /etc/sysconfig/network-scripts/ifcfg-eth0
-sudo sed -i '1d;2d;3d' /etc/sysconfig/network-scripts/ifcfg-eth1
-sudo systemctl stop NetworkManager
-sudo systemctl disable NetworkManager
-sudo chkconfig network on
-sudo touch /etc/sysconfig/network
-sudo reboot 0
