2752ea20e0cbb771c95e0ca58341b0b1b06ecdc4
Rafael-99-spec <60171771+Rafael-99-spec@users.noreply.github.com>
2020-05-27 07:05:50 -0700

    Update NFS-client-template.sh

--- NFS-client-template.sh
+++ NFS-client-template.sh
@@ -1,45 +1,40 @@
-    1  systemctl status network
-    2  reboot
-    3  cat /sbin/chkconfig 
-    4  systemctl status network
-    5  systemctl enable network
-    6  chkconfig network on
-    7  systemctl list-unit-files
-    8  systemctl list-unit-files | grep enabled
-    9  systemctl status network
-   10  reboot
-   11  journalctl -u network
-   12  cat /var/log/messages 
-   13  datetimectl
-   14  timedatectl
-   15  reboot
-   16  cat /var/log/messages 
-   17  timedatectl
-   18  reboot 0
-   19  cat /var/log/messages 
-   20  timedatectl
-   21  reboot 0
-   22  eit
-   23  exit
-   24  cat /var/log/messages 
-   25  systemctl | grep network
-   26  cat /etc/sysconfig/network-scripts/ifcfg-eth0 
-   27  cat /etc/sysconfig/network-scripts/ifcfg-eth1
-   28  vi /etc/ssh/sshd_config 
-   29  systemctl restart sshd
-   30  vi /etc/sysconfig/network-scripts/ifcfg-eth1
-   31  vi /etc/sysconfig/network-scripts/ifcfg-eth0
-   32  reboot 0
-   33  ip a
-   34  cat /etc/sysconfig/network-scripts/ifcfg-eth0
-   35  systemctl stop NetworkManager
-   36  touch /etc/sysconfig/network
-   37  systemctl disable NetworkManager
-   38  reboot 0
-   39  systemctl status network
-   40  df -h
-   41  reboot 0
-   42  systemctl status network
+    1  df -h
+    2  systemctl status network
+    3  sudo systemctl status network
+    4  reboot 0
+    5  sudo reboot 0
+    6  sudo systemctl status network
+    7  sudo systemctl status network -l
+    8  sudo vi /etc/sysconfig/network-scripts/ifcfg-eth0
+    9  sudo vi /etc/sysconfig/network-scripts/ifcfg-eth1
+   10  sudo systemctl restart network
+   11  sudo systemctl restart NetworkManager
+   12  sudo reboot 0
+   13  sudo systemctl status network -l
+   14  sudo vi /etc/sysconfig/network-scripts/ifcfg-eth0
+   15  sudo vi /etc/sysconfig/network-scripts/ifcfg-eth1
+   16  ip a
+   17  sudo vi /etc/sysconfig/network-scripts/ifcfg-eth1
+   18  sudo vi /etc/sysconfig/network-scripts/ifcfg-eth0
+   19  sudo vi /etc/sysconfig/network-scripts/ifcfg-eth1
+   20  sudo vi /etc/sysconfig/network-scripts/ifcfg-eth0
+   21  nmcli device status
+   22  sudo cat /etc/sysconfig/network-scripts/ifcfg-eth0
+   23  sudo cat /etc/sysconfig/network-scripts/ifcfg-eth1
+   24  systemctl stop NetworkManager
+   25  sudo systemctl stop NetworkManager
+   26  sudo systemctl disable NetworkManager
+   27  sudo systemctl enable network
+   28  sudo chkconfig network on
+   29  sudo systemctl enable network
+   30  sudo /sbin/chkconfig network on
+   31  sudo systemctl enable network
+   32  sudo /sbin/chkconfig network on
+   33  sudo systemctl start network
+   34  sudo touch /etc/sysconfig/network
+   35  sudo systemctl restart network
+   36  history
+
    
    
    Attempt 3
