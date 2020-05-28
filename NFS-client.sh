e09540a8b1862e675c8a3772ec82308fe7e78781
Rafael-99-spec <60171771+Rafael-99-spec@users.noreply.github.com>
2020-05-28 01:46:10 -0700

    Update NFS-client.sh

--- NFS-client.sh
+++ NFS-client.sh
@@ -1,16 +1,11 @@
 sudo yum -y install nfs-utils
 sudo systemctl start rpcbind
 sudo systemctl enable rpcbind
+sudo systemctl stop network
+sudo chkconfig network off
 cd /
-sudo mkdir /mount
-sudo chmod 766 /etc/fstab
-sudo echo "192.168.11.101:/mount  /mount		nfs        _netdev,x-systemd.automount,x-systemd.requires=network-online.target,x-systemd.device-timeout=10 0 0" >> /etc/fstab
-sudo mount -t nfs 192.168.11.101:/mount /mount
-sudo sed -i '5d' /etc/sysconfig/network-scripts/ifcfg-eth0
-sudo sed -i '1d;2d;3d' /etc/sysconfig/network-scripts/ifcfg-eth1
-sudo systemctl start network
-sudo systemctl stop NetworkManager
-sudo systemctl disable NetworkManager
-sudo systemctl restart network
-sudo chkconfig network on
-sudo touch /etc/sysconfig/network
+sudo mkdir -p /shared/upload
+sudo chmod 766 /shared
+sudo chmod 766 /shared/upload
+sudo cp /vagrant/shared-upload.mount /etc/systemd/system/shared-upload.mount
+sudo systemctl daemon-reload
