c4647c18a199d6246c41907c26fc4c6781dc4762
Rafael-99-spec <60171771+Rafael-99-spec@users.noreply.github.com>
2020-05-29 16:38:20 +0200

    Update NFS-client.sh

--- NFS-client.sh
+++ NFS-client.sh
@@ -8,4 +8,9 @@ sudo mkdir -p /shared/upload
 sudo chmod 766 /shared
 sudo chmod 766 /shared/upload
 sudo cp /vagrant/shared-upload.mount /etc/systemd/system/shared-upload.mount
+sudo cp /vagrant/network-online.service /etc/systemd/system/netowrk-service.online
 sudo systemctl daemon-reload
+sudo systemctl start netowrk-service.online
+sudo systemctl enable network-service.online
+sudo systemctl start shared-upload.mount
+sudo systemctl enable shared-upload.mount
